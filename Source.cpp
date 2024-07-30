38383207b021eb3496b78a287a10735b1273f893
Chuck Norris <me@example.com>
2024-07-30 15:06:06 +0200

    succesfully integrated

--- Source.cpp
+++ Source.cpp
@@ -175,67 +175,6 @@ private:
 		return fractX < 0.1 || fractX > 0.9 || fractY < 0.1 || fractY > 0.9;
 	}
 
-
-
-	struct RayInfo { bool hit; float distance; bool edge; } shootRay(Player& player, float angle) {
-		float direction[2] = { cosf(angle), sinf(angle) };
-
-		RayInfo result = { false, INFINITY, false };
-
-//		const float step = 0.01;
-//		float d = step;
-//		while (d < MAX_DISTANCE) {
-//			int x = direction[0] * d + player.x;
-//			int y = direction[1] * d + player.y;
-//			if (x >= W || x < 0 || y >= H || y < 0) return result;
-//			if (gameMap[y][x] == 1) {
-//				result.distance = d;
-//				result.hit = true;
-//				return result;
-//			}
-//;			d += step;
-//		}
-//
-//		return result;
-
-		float line[3] = { direction[1],-direction[0], -direction[1] * player.x + direction[0] * player.y };
-		
-		//cout << line[0] << ' ' << line[1] << ' ' << line[2] << endl;
-		//cout << direction[0] << ' ' << direction[1] << endl;
-
-		
-
-		// Check horizontal intersections
-		if (direction[1] > 0) {
-			for (float y = ceil(player.y); y <H; y++) {
-				float x = (-line[1] * y - line[2]) / line[0];
-				raycastLogic(line, x, y, result);
-			}
-		}
-		else {
-			for (float y = floor(player.y); y>=0; y--) {
-				float x = (-line[1] * y - line[2]) / line[0];
-				raycastLogic(line, x, y, result, 0, -1);
-			}
-		}
-
-		// Check vertical intersect
-		if (direction[0] > 0) {
-			for (float x = ceil(player.x); x < W; x++) {
-				float y = (-line[0] * x - line[2]) / line[1];
-				raycastLogic(line, x, y, result);
-			}
-		}
-		else {
-			for (float x = floor(player.x); x >= 0; x--) {
-				float y = (-line[0] * x - line[2]) / line[1];
-				raycastLogic(line, x, y, result, -1);
-			}
-		}
-
-		return result;
-	}
-
 	void drawMap() {
 		for (int x = 0; x < W; x++) {
 			for (int y = 0; y < H; y++) {
@@ -252,8 +191,8 @@ private:
 
 		float a = player.angle - HFOV;
 		for (int i = 0; i < rayCount; i++) {
-			RayInfo result = shootRay(player, a);
-			if (result.hit) {
+			RaycastResult result = shootRay(a);
+			if (result.bHit) {
 				//cout << "yes " <<  result.distance << ' ' << i << endl;
 				DrawLine(olc::vi2d(player.x * 10, player.y * 10), olc::vi2d(cosf(a) * result.distance*10 + player.x * 10, sinf(a) * result.distance*10 + player.y * 10), olc::GREEN);
 			}
@@ -262,32 +201,18 @@ private:
 		//DrawCircle()
 	}
 
-	void raycastLogic(float line[], float x, float y, RayInfo& result, float offX=0, float offY=0) {
-		if (x+offX >= W || x+offX < 0 || y+offY >= H || y+offY < 0) return;
-		if (gameMap[(int)y+offY][(int)x+offX] == 1) {
-			float d = dist(x, y, player.x, player.y);
-			if (d < result.distance) {
-				result.distance = d;
-				result.hit = true;
-				float dx = x - (int)x;
-				float dy = y - (int)y;
-				result.edge = (dx == 0 && (dy < 0.1 || dy > 0.9)) || (dy == 0 && (dx < 0.1 || dx > 0.9));
-			}
-		}
+	RaycastResult shootRay(float angle) {
+		return cast_ray(olc::vf2d(player.x, player.y), olc::vf2d(cosf(angle), sinf(angle)), gameMap, olc::vi2d(W, H), MAX_DISTANCE);
 	}
 
 
-	void drawRay(RayInfo& info, int i) {
+	void drawRay(RaycastResult& info, int i) {
 		//olc::Pixel color = olc::Pixel((1-info.distance / MAX_DISTANCE)*255);
 		//auto color = olc::WHITE;
 		float perc = max(0.0f, powf((1 - info.distance / MAX_DISTANCE),2));
 		olc::Pixel color;
-		if (info.edge) {
-			color = olc::GREEN;
-		}
-		else {
-			color = olc::Pixel(perc * 255, perc * 255, perc * 255);
-		}
+		color = olc::Pixel(perc * 255, perc * 255, perc * 255);
+		
 		float dx = (float)ScreenWidth() / (float)rayCount;
 		float x =  dx * (float) i;
 
@@ -301,8 +226,8 @@ private:
 	void raycast() {
 		float a = player.angle - HFOV;
 		for (int i = 0; i < rayCount; i++) {
-			RayInfo result = shootRay(player, a);
-			if (result.hit) {
+			RaycastResult result = shootRay(a);
+			if (result.bHit) {
 				//cout << "yes " <<  result.distance << ' ' << i << endl;
 				drawRay(result, i);
 			}
@@ -360,7 +285,7 @@ int main()
 	//Game demo;
 	//if (demo.Construct(800, 600, 2, 2))
 	//	demo.Start();
-	RaycastDebug window;
+	Game window;
 	if (window.Construct(600, 600, 1, 1)) {
 		window.Start();
 	}
